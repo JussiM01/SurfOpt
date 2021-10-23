cdc911bb2e537c2b00ba7da59b8f5a8895f546a1
jussi <jussi.h.a.martin@gmail.com>
2021-10-23 22:50:56 +0300

    Add test for GaussMonom.

--- tests/test_surfacemap.py
+++ tests/test_surfacemap.py
@@ -16,9 +16,28 @@ testdata0 = [
 
 tensor0 = torch.Tensor([[-2.0, 3.0]]).to(device)
 
+
 @pytest.mark.parametrize("constant,powers,expected", testdata0)
 def test_monomial(constant, powers, expected):
     model = Monomial({'const': constant, 'pow': powers})
     result = model(tensor0)
 
     assert expected == result
+
+
+testdata1 = [
+    [[-1.0, 1.0], torch.exp(torch.Tensor([0.0, -2.0, -8.0]))],
+    [[0.0, 0.0], torch.exp(torch.Tensor([-2.0, 0.0, -2.0]))],
+    [[1.0, -1.0], torch.exp(torch.Tensor([-8.0, -2.0, 0.0]))]
+]
+
+tensor1 = torch.Tensor([[-1.0, 1.0] , [0.0, 0.0], [1.0, -1.0]])
+
+
+@pytest.mark.parametrize("mean,expected", testdata1)
+def test_gaussmonom(mean, expected):
+    params = {'mean': mean, 'cov': [[1.0, 0.0], [0.0, 1.0]], 'const': [1.0]}
+    model = GaussMonom(params)
+    result = model(tensor1)
+
+    assert torch.equal(expected, result)
