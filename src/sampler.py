9887d4b42cd7c9df380ff3797dc801d3d90b575c
jussi <jussi.h.a.martin@gmail.com>
2021-10-29 00:26:06 +0300

    Add section for sampler's testing with trajectories plots.

--- src/sampler.py
+++ src/sampler.py
@@ -40,3 +40,36 @@ class Sampler:
         else:
             self.trajectories = np.concatenate(
                 (self.trajectories, trajectories), axis=0)
+
+
+if __name__ == '__main__':
+
+    import argparse
+
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-ua', '--use_arcs', type=bool, default=True)
+    parser.add_argument('-na', '--num_angles', type=int, default=9)
+    parser.add_argument('-mi', '--min_angle', type=int, default=np.pi/18)
+    parser.add_argument('-ma', '--max_angle', type=int, default=np.pi/18)
+    parser.add_argument('-ns', '--num_steps', type=int, default=50)
+
+    args = parser.parse_args()
+
+    if args.use_arcs is True:
+
+        params = {
+            'num_angles': args.num_angles,
+            'min_angle': args.min_angle,
+            'max_angle': args.max_angle,
+            'num_steps': args.num_steps
+        }
+
+    elif:
+         raise NotImplementedError
+
+    sampler = Sampler(params)
+    trajectories = sampler()
+
+    # Plot the trajectories here.
