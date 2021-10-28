38483ab87989dcf546a1632cd48560fed13e1777
jussi <jussi.h.a.martin@gmail.com>
2021-10-29 00:27:42 +0300

    Sampler's helper ready to be tested/debugged.

--- src/utils.py
+++ src/utils.py
@@ -22,22 +22,44 @@ def sample_arcs(params):
     num_steps = params['num_steps']
     angles_signs = [(angle, sign) for angle in angles for sign in [-1.0, 1.0]]
 
-    return np.array([create_arc(angle, num_steps, start_point, end_point, sign)
-                     for angle, sign in angles_signs])
+    arcs = np.stack([create_arc(angle, num_steps, start_point, end_point, sign)
+                     for angle, sign in angles_signs], axis=0)
 
-def create_arc(angle, num_steps, start_point, end_point):
-
-    return [arc_point(step, num_steps, angle, start_point, end_point, sign)
-            for i in range(num_steps)]
+    return arcs
 
 
-def arc_point(step, num_steps, angle, start_point, end_point, sign):
+def create_arc(angle, num_steps, start, end):
 
-    if start_point[1] == end_point[1]:
-        raise NotImplementedError
+    arc = np.stack(
+        [arc_point(step, num_steps, angle, start, end, sign)
+         for i in range(num_steps)], axis=0)
+
+    return arc
+
+
+def arc_point(step, num_steps, angle, start, end, sign):
+
+    step_size = angle/(num_steps -1)
+    alpha = step_size*step -angle*0.5
+    radius = 1/np.sin(angle)
+    x_value = radius*(np.cos(alpha) - np.cos(angle))
+    y_value = radius*np.sin(alpha)
+
+    if start[0] == end[0]:
+        vector = sign*np.array([x_value, y_value])
 
     else:
-        raise NotImplementedError
+        angle_from_x_axis = np.arctan((end[0] -start[0])/(end[1] -start[1]))
+        vector = rotate(-angle_from_x_axis, sign*np.array([y_value, x_value]))
+
+    return vector
+
+
+def rotate(alpha, point):
+    rotation_matrix = np.array(
+        [[np.cos(alpha), -np.sin(alpha)], [np.sin(alpha), np.cos(alpha)])
+
+    return rotation_matrix.dot(point)
 
 
 def sample_sines(params):
