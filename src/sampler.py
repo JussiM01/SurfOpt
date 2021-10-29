456fcaaa8d20b4ee44db41576cd3b20f42239e35
jussi <jussi.h.a.martin@gmail.com>
2021-10-30 01:03:30 +0300

    Add plotting functionality.

--- src/sampler.py
+++ src/sampler.py
@@ -46,6 +46,8 @@ if __name__ == '__main__':
 
     import argparse
 
+    import matplotlib.pyplot as plt
+
 
     parser = argparse.ArgumentParser()
 
@@ -54,22 +56,44 @@ if __name__ == '__main__':
     parser.add_argument('-mi', '--min_angle', type=int, default=np.pi/18)
     parser.add_argument('-ma', '--max_angle', type=int, default=np.pi/18)
     parser.add_argument('-ns', '--num_steps', type=int, default=50)
+    parser.add_argument('-x0', '--start_x', type=float, default=-1.0)
+    parser.add_argument('-y0', '--start_y', type=float, default=0.0)
+    parser.add_argument('-x1', '--end_x', type=float, default=-1.0)
+    parser.add_argument('-y1', '--end_y', type=float, default=0.0)
 
     args = parser.parse_args()
 
+    params = {}
+
     if args.use_arcs is True:
 
-        params = {
+        params['arcs'] = {
             'num_angles': args.num_angles,
             'min_angle': args.min_angle,
             'max_angle': args.max_angle,
-            'num_steps': args.num_steps
+            'num_steps': args.num_steps,
+            'start': [args.start_x, args.start_y],
+            'end': [args.end_x, args.end_y]
         }
 
-    elif:
+    else:
          raise NotImplementedError
 
     sampler = Sampler(params)
     trajectories = sampler()
 
-    # Plot the trajectories here.
+    max_value = max(abs(np.min(trajectories)), np.max(trajectories))
+    bound = max_value + 1
+
+    fig = plt.figure(figsize=(7, 7))
+    ax = fig.add_axes([0.1, 0.1, 0.8, 0.8], frameon=True)
+    ax.set_xlim(-bound, bound)
+    ax.set_ylim(-bound, bound)
+    ax.grid(True)
+
+    for i in range(trajectories.shape[0]):
+        x_values = trajectories[i, :, 0]
+        y_values = trajectories[i, :, 1]
+        ax.plot(x_values, y_values)
+
+    plt.show()
